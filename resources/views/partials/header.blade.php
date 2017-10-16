53b4c0cbf6c224a06b2145aa6ae61b0f973354b0
Cpazek <Cpazek@gmail.com>
2017-10-15 17:00:01 -0700

    included new css dps

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -9,6 +9,9 @@
 
 <!-- Styles -->
 <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
+
 <link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet">
+<link href="{{ asset('css/hover.css') }}" rel="stylesheet">
+<link href="{{ asset('css/morebuttons.css') }}" rel="stylesheet">
 
 
