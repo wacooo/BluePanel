c685aaf5d416b69bf4ea40dd786ec30b87440c56
volxz <ethan@exclnetworks.com>
2017-10-23 10:39:21 -0500

    Update sidenav to contain kiosk list logic

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -6,56 +6,58 @@
         </div>
         <div class="pull-left info">
             @guest
-            <p>Guest</p>
+                <p>Guest</p>
             @endguest
             @auth
-            <p>{{ Auth::user()->name }}</p>
-                @endauth
+                <p>{{ Auth::user()->name }}</p>
+            @endauth
 
         </div>
     </div>
 
     <!-- sidebar menu: : style can be found in sidebar.less -->
     <ul class="sidebar-menu" data-widget="tree">
-
+        s
 
         <li class="treeview">
             <a href="#">
-                <i class="fa fa-user-secret"></i> <span>Current Students</span>
+                <i class="fa fa-user-secret"></i> <span>Present Students</span>
                 <span class="pull-right-container">
-                    <i class="fa fa-angle-left pull-right"></i>
-
             </span>
             </a>
             <ul class="treeview-menu">
-                <li><a href="../../index.html"><i class="fa fa-circle-o"></i> Kiosks 1</a></li>
-                <li><a href="../../index2.html"><i class="fa fa-circle-o"></i> Kiosks 2</a></li>
+
             </ul>
         </li>
         <li class="treeview">
             <a href="#">
                 <i class="fa fa-clock-o"></i> <span>Recent Students</span>
                 <span class="pull-right-container">
-
-            </span>
-            </a>
-
-        </li>
-        @auth
-        <li class="header">User Tasks</li>
-        <li class="treeview">
-            <a href="#">
-                <i class="fa fa-desktop"></i> <span>My Kiosks</span>
-                <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
+                    <i class="fa fa-angle-left pull-right"></i>
             </span>
             </a>
             <ul class="treeview-menu">
-                <li><a href="../../index.html"><i class="fa fa-circle-o"></i> Kiosks 1</a></li>
-                <li><a href="../../index2.html"><i class="fa fa-circle-o"></i> Kiosks 2</a></li>
+                @foreach(\App\Kiosk::all() as $kiosk)
+                    <li><a href="../../index.html"><i class="fa fa-circle-o"></i>{{$kiosk->name}}</a></li>
+                @endforeach
             </ul>
-
         </li>
+        @auth
+            <li class="header">User Tasks</li>
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-desktop"></i> <span>My Kiosks</span>
+                    <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+                </a>
+                <ul class="treeview-menu">
+                    @foreach(Auth::user()->kiosks as $kiosk)
+                        <li><a href="../../index.html"><i class="fa fa-circle-o"></i>{{$kiosk->name}}</a></li>
+                    @endforeach
+                </ul>
+
+            </li>
 
             @if ( Auth::user()->isAdministrator())
 
@@ -63,40 +65,36 @@
                 <li class="header">Administrative Tasks</li>
 
 
-            <li class="treeview">
-                <a href="#">
-                    <i class="fa fa-database"></i> <span>Kiosk Management</span>
-                    <span class="pull-right-container">
-
+                <li class="treeview">
+                    <a href="#">
+                        <i class="fa fa-database"></i> <span>Kiosk Management</span>
+                        <span class="pull-right-container">
 
             </span>
-                </a>
+                    </a>
 
-            </li>
-            <li class="treeview">
-                <a href="#">
-                    <i class="fa fa-user"></i> <span>User Management</span>
-                    <span class="pull-right-container">
+                </li>
+                <li class="treeview">
+                    <a href="#">
+                        <i class="fa fa-user"></i> <span>User Management</span>
+                        <span class="pull-right-container">
             </span>
-                </a>
+                    </a>
 
 
-            </li>
-            <li class="treeview">
-                <a href="#">
-                    <i class="fa fa-desktop"></i> <span>All Kiosks</span>
-                    <span class="pull-right-container">
+                </li>
+                <li class="treeview">
+                    <a href="#">
+                        <i class="fa fa-desktop"></i> <span>Kiosks</span>
+                        <span class="pull-right-container">
               <i class="fa fa-angle-left pull-right"></i>
             </span>
-                </a>
-                <ul class="treeview-menu">
-                    <li><a href="../../index.html"><i class="fa fa-circle-o"></i> Kiosks 1</a></li>
-                    <li><a href="../../index2.html"><i class="fa fa-circle-o"></i> Kiosks 2</a></li>
-                </ul>
-
-            </li>
-
-        @endif
+                    </a>
+                    <ul class="treeview-menu">
+                        <li><a href="../../index.html"><i class="fa fa-circle-o"></i>{{$kiosk->name}}</a></li>
+                    </ul>
+                </li>
+            @endif
         @endauth
 
     </ul>
