ec73a14d23c3731961e7ae088654bdadc66d68c7
Cpazek <Cpazek@gmail.com>
2017-10-22 18:57:50 -0700

    create scaffolding for users page and update routes

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('/kiosk', 'KioskController');
+Route::get('/users', function () {
+    return view ('users');
+});
