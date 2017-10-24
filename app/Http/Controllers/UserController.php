0c49525afb8784c4bfcb29ad2f605aacce5a4c87
volxz <ethan@exclnetworks.com>
2017-10-24 13:15:34 -0500

    make usercontroller methods directly intake user eloquent models

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,88 +1,91 @@
-<?php 
+<?php
 
 namespace App\Http\Controllers;
-use App\User;
 
-class UserController extends Controller 
+use App\User;
+use App\Kiosk;
+
+class UserController extends Controller
 {
 
-  /**
-   * Display a listing of the resource.
-   *
-   * @return Response
-   */
-  public function index()
-  {
-      $users = User::all();
-      return view('users')->with('users', $users);
-  }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        $users = User::all();
+        return view('users')->with('users', $users);
+    }
 
-  /**
-   * Show the form for creating a new resource.
-   *
-   * @return Response
-   */
-  public function create()
-  {
-    
-  }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
 
-  /**
-   * Store a newly created resource in storage.
-   *
-   * @return Response
-   */
-  public function store()
-  {
-    
-  }
+    }
 
-  /**
-   * Display the specified resource.
-   *
-   * @param  int  $id
-   * @return Response
-   */
-  public function show($id)
-  {
-    
-  }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @return Response
+     */
+    public function store()
+    {
 
-  /**
-   * Show the form for editing the specified resource.
-   *
-   * @param  int  $id
-   * @return Response
-   */
-  public function edit($id)
-  {
-    $user = User::findOrFail($id)->with('kiosks')->first();
-    return view('edituser')->with('user', $user);
+    }
 
-  }
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function show(User $user)
+    {
 
-  /**
-   * Update the specified resource in storage.
-   *
-   * @param  int  $id
-   * @return Response
-   */
-  public function update($id)
-  {
-    
-  }
+    }
 
-  /**
-   * Remove the specified resource from storage.
-   *
-   * @param  int  $id
-   * @return Response
-   */
-  public function destroy($id)
-  {
-    
-  }
-  
+    /**
+     * Show the form for editinSSg the specified resource.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function edit(User $user)
+    {
+        return view('admin.edituser')->with(
+            'user', $user::with('kiosks'
+        )->first());
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function update($id)
+    {
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+
+    }
+
 }
 
 ?>
