b4fa631151ddb866f7ed55f232e3a2a3e26f0cc1
volxz <ethan@exclnetworks.com>
2017-11-14 10:41:21 -0600

    Create kiosk times migration

---
+++ database/migrations/2017_11_08_125537_CreateKioskScheduleTable.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateKioskScheduleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kiosk_schedule', function(Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->integer('kiosk_id')->unsigned();
+            $table->time('time')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
