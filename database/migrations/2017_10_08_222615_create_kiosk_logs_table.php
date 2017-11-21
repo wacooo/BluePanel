095ad66b1c22596586d602bbe2ed76b53f43364a
volxz <ethan.gallant@gmail.com>
2017-11-20 19:18:38 -0500

    Make log table plural

--- database/migrations/2017_10_08_222615_create_kiosk_logs_table.php
+++ database/migrations/2017_10_08_222615_create_kiosk_logs_table.php
@@ -3,11 +3,11 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateKioskLogTable extends Migration {
+class CreateKioskLogsTable extends Migration {
 
 	public function up()
 	{
-		Schema::create('kiosk_log', function(Blueprint $table) {
+		Schema::create('kiosk_logs', function(Blueprint $table) {
 			$table->increments('id')->unsigned();
 			$table->timestamps();
 			$table->integer('kiosk_id')->unsigned();
@@ -17,6 +17,6 @@ class CreateKioskLogTable extends Migration {
 
 	public function down()
 	{
-		Schema::drop('kiosk_log');
+		Schema::drop('kiosk_logs');
 	}
 }
