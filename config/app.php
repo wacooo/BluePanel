c609995294514081cff14b188f9169d0e4286958
volxz <ethan.gallant@gmail.com>
2017-11-20 19:19:23 -0500

    Add Yara/Datatables dependency

--- config/app.php
+++ config/app.php
@@ -176,6 +176,9 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        //Yara Datatables
+        Yajra\DataTables\DataTablesServiceProvider::class,
+
 
     ],
 
@@ -225,6 +228,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'DataTables' => Yajra\DataTables\Facades\DataTables::class,
+
 
     ],
 
