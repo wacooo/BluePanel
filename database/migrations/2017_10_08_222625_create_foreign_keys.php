7cb2c761bec952b13ddde3749dccfb7d3895d0e5
volxz <ethan.gallant@gmail.com>
2017-11-20 19:26:11 -0500

    Update foreign keys

--- database/migrations/2017_10_08_222625_create_foreign_keys.php
+++ database/migrations/2017_10_08_222625_create_foreign_keys.php
@@ -28,12 +28,12 @@ class CreateForeignKeys extends Migration {
 						->onDelete('cascade')
 						->onUpdate('cascade');
 		});
-		Schema::table('kiosk_log', function(Blueprint $table) {
+		Schema::table('kiosk_logs', function(Blueprint $table) {
 			$table->foreign('kiosk_id')->references('id')->on('kiosks')
 						->onDelete('restrict')
 						->onUpdate('restrict');
 		});
-		Schema::table('kiosk_log', function(Blueprint $table) {
+		Schema::table('kiosk_logs', function(Blueprint $table) {
 			$table->foreign('student_id')->references('id')->on('students')
 						->onDelete('cascade')
 						->onUpdate('cascade');
@@ -43,21 +43,21 @@ class CreateForeignKeys extends Migration {
 	public function down()
 	{
 		Schema::table('kiosk_user', function(Blueprint $table) {
-			$table->dropForeign('kiosk_users_user_id_foreign');
+			$table->dropForeign('kiosk_user_user_id_foreign');
 		});
 		Schema::table('kiosk_user', function(Blueprint $table) {
-			$table->dropForeign('kiosk_users_kiosk_id_foreign');
+			$table->dropForeign('kiosk_user_kiosk_id_foreign');
 		});
 		Schema::table('kiosk_student', function(Blueprint $table) {
-			$table->dropForeign('kiosk_students_student_id_foreign');
+			$table->dropForeign('kiosk_student_student_id_foreign');
 		});
 		Schema::table('kiosk_student', function(Blueprint $table) {
-			$table->dropForeign('kiosk_students_kiosk_id_foreign');
+			$table->dropForeign('kiosk_student_kiosk_id_foreign');
 		});
-		Schema::table('kiosk_log', function(Blueprint $table) {
+		Schema::table('kiosk_logs', function(Blueprint $table) {
 			$table->dropForeign('kiosk_logs_kiosk_id_foreign');
 		});
-		Schema::table('kiosk_log', function(Blueprint $table) {
+		Schema::table('kiosk_logs', function(Blueprint $table) {
 			$table->dropForeign('kiosk_logs_student_id_foreign');
 		});
 	}
