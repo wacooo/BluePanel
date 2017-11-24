a365c3445bba345736a6c4a93ec327a8d5fbf882
volxz <ethan.gallant@gmail.com>
2017-11-24 00:15:39 -0500

    Catch database not existing

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,18 +27,27 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $kioskSched = [];
-        if (Schema::hasTable('kiosk_schedule')) {
-            $kioskSched = DB::table('kiosk_schedule')->get();
+        // Test database connection
+        try {
+            DB::connection()->getPdo();
+            $kioskSched = [];
+            if (Schema::hasTable('kiosk_schedule')) {
+                $kioskSched = DB::table('kiosk_schedule')->get();
+            }
+
+            foreach ($kioskSched as $entry) {
+                //trim off mysql seconds
+                $schedule->command('kiosk:signoutstudents ' . $entry->id)
+                    ->dailyAt($entry->time);
+            }
+
+            $schedule->command('studentdb:update')
+                ->weekdays()->at('23:00');
+
+        } catch (\Exception $e) {
+            //We dont care if it fails
         }
 
-        foreach ($kioskSched as $entry) {
-            //trim off mysql seconds
-            $schedule->command('kiosk:signoutstudents '.$entry->id)
-                ->dailyAt($entry->time);
-        }
-        $schedule->command('studentdb:update')
-            ->weekdays()->at('23:00');
     }
 
     /**
@@ -48,7 +57,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
