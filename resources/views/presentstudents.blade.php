99557124247e6ba80a26e17cfc3f2136860b1213
Salamander2 <mk_gecko@yahoo.com>
2017-11-25 12:49:54 -0500

    Improved home screen formatting.

--- resources/views/presentstudents.blade.php
+++ resources/views/presentstudents.blade.php
@@ -4,6 +4,7 @@
 @section('content-header')
 @endsection
 @section('content')
+<!-- I don't think that any of this stuff is needed. The bootstrap col-lg-6 works fine as does AdminLTE box.  MH.
     <style>
         .col-lg-6 {
             float:none;
@@ -13,10 +14,11 @@
         }
 
     </style>
+-->
         @foreach(App\Kiosk::all() as $kiosk)
             <div class="col-lg-6">
 
-                <div class="box">
+                <div class="box box-solid box-primary">
                     <div class="box-header with-border">
                         <h3 class="box-title">{{$kiosk->name}}</h3>
                     </div>
